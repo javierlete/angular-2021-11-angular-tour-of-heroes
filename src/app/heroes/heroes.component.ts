4d69c483c4711eca10a36f41b6668db426d3f158
Javier <javier@eskura21.com>
2021-11-18 20:12:38 +0100

    Servicio getHeroes Observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -7,9 +8,23 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) { }
+  
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+
+    // this.heroService.getHeroes().subscribe(
+    //   (function(heroes: Hero[]) { this.heroes = heroes }).bind(this)
+    // );
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
